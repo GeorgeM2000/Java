0694bb3a38e15accbda32805be813d2463543fc2
George M <giorgos_matlis@hotmail.com>
2021-06-04 23:01:36 +0300

    Update TictactoeFunctionality.java

--- javaAdvanced_Projects/TicTacToe_Application/TictactoeFunctionality.java
+++ javaAdvanced_Projects/TicTacToe_Application/TictactoeFunctionality.java
@@ -14,9 +14,7 @@ public class TictactoeFunctionality {
 		else if(aiPlayer.equals("O")) globalPlayer = -1;
 	}
 	
-
-	// Returns the maximum element of the < array >
-	public Integer MAXIMUM(ArrayList<Integer> array) {
+	private Integer MAXIMUM(ArrayList<Integer> array) {
 		Integer max = array.get(0);
 		for(int i = 1;i < array.size();i++) {
 			if(array.get(i) > max) max = array.get(i);
@@ -24,10 +22,16 @@ public class TictactoeFunctionality {
 		return max;
 	}
 	
-
-	// When the computer makes a move, take the coordinates of the move(a move consists of two coordinates, x and y)
-	// and convert them into a button number
-	public int convertCoordinatesToButtonNum(List<Integer> coordinates) {
+	private Integer MINIMUM(ArrayList<Integer> array) {
+		Integer min = array.get(0);
+		for(int i = 1;i < array.size();i++) {
+			if(array.get(i) < min) min = array.get(i);
+		}
+		return min;
+	}
+	
+	
+	private int convertCoordinatesToButtonNum(List<Integer> coordinates) {
 		if(coordinates.get(0) == 0 && coordinates.get(1) == 0) return 1;
 		else if(coordinates.get(0) == 0 && coordinates.get(1) == 1) return 2;
 		else if(coordinates.get(0) == 0 && coordinates.get(1) == 2) return 3;
@@ -40,19 +44,8 @@ public class TictactoeFunctionality {
 		return 0;
 	}
 	
-
-	// Returns the minimum element of the < array >.
-	public Integer MINIMUM(ArrayList<Integer> array) {
-		Integer min = array.get(0);
-		for(int i = 1;i < array.size();i++) {
-			if(array.get(i) < min) min = array.get(i);
-		}
-		return min;
-	}
 	
-
-	// Returns all possible actions given state < s >.
-	public ArrayList<List<Integer>> ACTIONS(String[][] state) {
+	private ArrayList<List<Integer>> ACTIONS(String[][] state) {
 		ArrayList<List<Integer>> actions = new ArrayList<List<Integer>>();
 		for(int i = 0;i < 3;i++) {
 			for(int j = 0;j < 3;j++) {
@@ -66,8 +59,6 @@ public class TictactoeFunctionality {
 		return actions;
 	}
 	
-
-	// Returns the winner of the game. The winner is decided based on the current state < s >.
 	public int UTILITY(String[][] state) {
 		for(int i = 0;i < 3;i++) {
 			if(state[i][0].equals("X") && state[i][1].equals("X") && state[i][2].equals("X")) return 1;
@@ -88,8 +79,6 @@ public class TictactoeFunctionality {
 		return 0;
 	}
 	
-
-	// Returns < true > if the game is over or < false > if the game is not over.
 	public boolean TERMINAL(String state[][]) {
 		int gameover = UTILITY(state);
 		if(gameover == 1 || gameover == -1) return true;
@@ -103,9 +92,7 @@ public class TictactoeFunctionality {
 		return true;
 	}
 	
-
-	// Returns the state after action < a > is taken in state < s >.
-	public String[][] RESULT(String[][] state,int player,Integer[] action) {
+	private String[][] RESULT(String[][] state,int player,Integer[] action) {
 		String[][] newState = {{"0","0","0"},{"0","0","0"},{"0","0","0"}};
 		for(int i = 0;i < 3;i++) {
 			for(int j = 0;j < 3;j++) {
@@ -119,9 +106,7 @@ public class TictactoeFunctionality {
 		return newState;
 	}
 	
-
-	// Picks action a in ACTIONS(s) that produces the highest value of MIN_VALUE(RESULT(s,a))
-	public int MAX_VALUE(String[][] state, int player, int globalMINLevel, int globalMAXLevel) {
+	private int MAX_VALUE(String[][] state, int player, int globalMINLevel, int globalMAXLevel) {
 		if(TERMINAL(state)) return UTILITY(state);
 		int v = -10000;
 		Integer[] array;
@@ -139,9 +124,7 @@ public class TictactoeFunctionality {
 		return v;
 	}
 	
-
-	// Picks action a in ACTIONS(s) that produces the lowest value of MIN_VALUE(RESULT(s,a))
-	public int MIN_VALUE(String[][] state, int player, int globalMINLevel, int globalMAXLevel) {
+	private int MIN_VALUE(String[][] state, int player, int globalMINLevel, int globalMAXLevel) {
 		if(TERMINAL(state)) return UTILITY(state);
 		int v = 10000;
 		Integer[] array;
@@ -160,23 +143,25 @@ public class TictactoeFunctionality {
 	}
 	
 	
-	// Call MAX_VALUE or MIN_VALUE according to the player variable
 	public int MINIMAX(String[][] state) {
 		int player = globalPlayer;
-		if(player == 1) {																// Computer has X symbol
+		if(player == 1) {
+			@SuppressWarnings("unused")
 			int n = MAX_VALUE(state, player, globalMINLevel, globalMAXLevel+1);
 			ArrayList<List<Integer>> action = ACTIONS(state);
-			List<Integer> tmpList = action.get(maxArray.indexOf(MAXIMUM(maxArray))); 
+			List<Integer> tmpList = action.get(maxArray.indexOf(MAXIMUM(maxArray)));
 			maxArray.clear();
 			return convertCoordinatesToButtonNum(tmpList);
 		}
-		else {																			// Computer has O symbol
-			int n = MIN_VALUE(state, player, globalMINLevel+1, globalMAXLevel);			
+		else {
+			@SuppressWarnings("unused")
+			int n = MIN_VALUE(state, player, globalMINLevel+1, globalMAXLevel);
 			ArrayList<List<Integer>> action = ACTIONS(state);
 			List<Integer> tmpList = action.get(minArray.indexOf(MINIMUM(minArray)));
 			minArray.clear();
 			return convertCoordinatesToButtonNum(tmpList);
 		}
 	}
+	
 	
 }
